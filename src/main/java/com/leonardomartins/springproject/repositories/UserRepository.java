28b889e4b206216d08d1288de0b54b1b32d550e7
Leonardo Martins <leodsmartins@gmail.com>
2026-09-17 16:05:57 +0100

    Add Service layer, component registration

--- src/main/java/com/leonardomartins/springproject/repositories/UserRepository.java
+++ src/main/java/com/leonardomartins/springproject/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.leonardomartins.springproject.repositories;
 import com.leonardomartins.springproject.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
