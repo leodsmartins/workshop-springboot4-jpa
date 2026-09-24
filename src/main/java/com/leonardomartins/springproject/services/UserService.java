30d264b5e5d5ccf241b77a3fe6f0a580f13c6f58
Leonardo Martins <leodsmartins@gmail.com>
2026-09-24 14:36:09 +0100

    Add Exception handling - update

--- src/main/java/com/leonardomartins/springproject/services/UserService.java
+++ src/main/java/com/leonardomartins/springproject/services/UserService.java
@@ -5,6 +5,7 @@ import com.leonardomartins.springproject.entities.User;
 import com.leonardomartins.springproject.repositories.UserRepository;
 import com.leonardomartins.springproject.services.exceptions.DatabaseException;
 import com.leonardomartins.springproject.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+     try {
+         User entity = repository.getReferenceById(id);
+         updateData(entity, obj);
+         return repository.save(entity);
+     } catch (EntityNotFoundException e) {
+         throw new ResourceNotFoundException(id);
+     }
         
     }
 
